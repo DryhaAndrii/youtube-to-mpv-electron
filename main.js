8fe13cc4f8570c24bd225d2bf04a79d5455fbdcb
Andrii Dryha <driga.and@gmail.com>
2025-10-10 16:44:13 +0300

    Added support for Windows 7

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const { spawn } = require("child_process");
 
+// Disable GPU hardware acceleration to mitigate GPU process crashes on some systems
+app.disableHardwareAcceleration();
+// Force ANGLE backend to D3D11 for better compatibility on Windows
+app.commandLine.appendSwitch("use-angle", "d3d11");
+// Disable sandbox for better Windows 7 compatibility
+app.commandLine.appendSwitch("no-sandbox");
+// Disable GPU sandbox (helps on older Windows versions)
+app.commandLine.appendSwitch("disable-gpu-sandbox");
+
 // In dev mode (React dev server)
 const isDev = !app.isPackaged;
 const mpvPath = isDev
